56d74671f81f5eae512e817abfdbf2b96dea88f3
Timothy Ghanem <timothy.ghanim@hotmail.com>
2020-06-27 10:07:27 +0300

    creating simple tree.

--- src/main/java/windows/ActivitiesWindow.java
+++ src/main/java/windows/ActivitiesWindow.java
@@ -1,11 +1,28 @@
 package windows;
 
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
 
 public class ActivitiesWindow {
     private JPanel contentPanel;
+    private JTree components;
+
+    public ActivitiesWindow() {
+    }
 
     public JPanel getContent() {
         return contentPanel;
+    }
+
+    private void createUIComponents() {
+
+        DefaultMutableTreeNode root =
+                new DefaultMutableTreeNode("Main Activity");
+
+        root.add(new DefaultMutableTreeNode("onCreate"));
+        root.add(new DefaultMutableTreeNode("onResume"));
+        root.add(new DefaultMutableTreeNode("onStop"));
+
+        components = new JTree(root);
     }
 }
